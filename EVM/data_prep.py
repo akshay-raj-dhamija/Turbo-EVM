f12db32cbf9d07912c0cc0474f790cb3a7ba3d3c
akshay-raj-dhamija <akshay.raj.dhamija@gmail.com>
2020-09-09 05:30:53 -0600

    Working Multi-GPU version with RPC

--- EVM/data_prep.py
+++ EVM/data_prep.py
@@ -2,39 +2,63 @@ import time
 import math
 import h5py
 import torch
+from tqdm import tqdm
+# import tables
 
-def read_features(args, cls_to_process):
+def read_features(args, cls_to_process=None):
     try:
+        # h5_objs = [tables.open_file(file_name, driver="H5FD_CORE") for file_name in args.feature_files]
         h5_objs = [h5py.File(file_name, "r") for file_name in args.feature_files]
         file_layer_comb = list(zip(h5_objs, args.layer_names))
+        if cls_to_process is None:
+            cls_to_process = sorted(list(h5_objs[0].keys()))
+        if args.debug:
+            cls_to_process = cls_to_process[:50]
         for cls in cls_to_process:
             temp = []
             for hf, layer_name in file_layer_comb:
                 temp.append(torch.squeeze(torch.tensor(hf[cls][layer_name])))
             features = torch.cat(temp)
-            yield features
+            yield cls,features
     finally:
         for h in h5_objs:
             h.close()
 
-def prep_all_features(gpu, args):
-    # total number of classes
-    l = len(args.all_classes)
-    # batches of negative classes
-    c = list(range(0, l + l % args.world_size + 1, math.ceil(l / args.world_size)))
-    cls_to_process_ = args.all_classes[c[gpu] : c[gpu + 1]]
-    cls_to_process, cls_to_process_shapes = zip(*cls_to_process_)
-    features_gen = read_features(args, cls_to_process)
-    features = []
+
+def add_chunk(all_classes,per_chunk_features_t,starting_indxs_dict):
+    per_chunk_features_t = torch.cat(per_chunk_features_t)
+    per_chunk_features_norm = per_chunk_features_t.norm(p=2, dim=1, keepdim=True)
+    all_classes['features_t'].append(per_chunk_features_t.t())
+    all_classes['features_t_norm'].append(per_chunk_features_norm.t())
+    all_classes['start_indxs'].append(starting_indxs_dict)
+    return all_classes, 0,{},[]
+
+
+def prep_all_features(args):
+    features_gen = read_features(args)
+    all_classes={}
+    all_classes['features_t']=[]
+    all_classes['features_t_norm']=[]
+    all_classes['start_indxs']=[]
     start = 0
-    start_indxs = []
-    for feature in features_gen:
-        start_indxs.append(start)
-        features.append(feature)
+    per_chunk_features_t = []
+    starting_indxs_dict = {}
+    class_names = []
+    pbar = tqdm(total=1000)
+    for cls,feature in features_gen:
+        pbar.update(1)
+        per_chunk_features_t.append(feature)
+        starting_indxs_dict[cls] = start
         start += feature.shape[0]
-    start_indxs.append(start)
-    features = torch.cat(features)
-    features_norm = features.norm(p=2, dim=1, keepdim=True)
-    features_t = features.t().to(f"cuda:{gpu}")
-    features_norm_t = features_norm.t().to(f"cuda:{gpu}")
-    return features_t, features_norm_t, start_indxs
+        class_names.append(cls)
+        if len(per_chunk_features_t)%args.cls_per_chunk == 0:
+            all_classes, start, starting_indxs_dict, per_chunk_features_t = add_chunk(all_classes,
+                                                                                      per_chunk_features_t,
+                                                                                      starting_indxs_dict)
+    # Add any additional values missed
+    if len(per_chunk_features_t)>0:
+        all_classes, _, _, _ = add_chunk(all_classes,
+                                         per_chunk_features_t,
+                                         starting_indxs_dict)
+    pbar.close()
+    return all_classes, class_names
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import argparse
 import h5py
 import time
-import torch
-import torch.multiprocessing as mp
-import torch.distributed.rpc as rpc
 import EVM
 
 def command_line_options():
@@ -26,6 +23,7 @@ def command_line_options():
     parser.add_argument("-g", "--gpus", nargs="+", default=[1], type=int, help="number of gpus per node")
     parser.add_argument("--local_rank", default=0, type=int)
     parser.add_argument("--debug", help="debugging flag", action="store_true", default=False)
+    parser.add_argument("--cls_per_chunk", help="Number of Classes per distance computation chunk", default=100, type=int)
     parser.add_argument('--dist-url', default='localhost', type=str,
                         help='Masters IP address')
     args = parser.parse_args()
@@ -33,33 +31,7 @@ def command_line_options():
     return args
 
 def main(args):
-    h5_objs = [h5py.File(file_name, "r") for file_name in args.feature_files]
-    file_layer_comb = list(zip(h5_objs, args.layer_names))
-    args.all_classes = []
-    for cls in h5_objs[0].keys():
-        temp=[None,0]
-        for hf, layer_name in file_layer_comb:
-            if temp[0] is None:
-                temp[0] = hf[cls][layer_name].shape[0]
-            temp[1] += hf[cls][layer_name].shape[1]
-        args.all_classes.append((cls,temp))
-    for h in h5_objs: h.close()
-
-    if args.debug:
-        args.all_classes = args.all_classes[: args.world_size * 3]
-
-    if args.world_size == 1:
-        EVM.trainer(0, args)
-    else:
-        processes = []
-        for rank in range(args.gpus[args.local_rank]):
-            p = mp.Process(target=EVM.trainer, args=(rank, args))
-            p.start()
-            processes.append(p)
-
-    if args.world_size > 1:
-        for p in processes:
-            p.join()
+    EVM.trainer(args)
 
 if __name__ == "__main__":
     args = command_line_options()
